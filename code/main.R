761cdd8795eae75c2471efc32399d032ef5a1edc
Alexander B. Gedranovich <ales@openSUSE.site>
2014-09-26 00:44:37 +0300

    Basic analysis

--- code/main.R
+++ code/main.R
@@ -13,9 +13,34 @@
 #
 ###############################################################################
 
+# Clears the workspace
+rm(list = ls(all = T))
+gc(reset = T)
+set.seed(12345)
+
+# Libraries
 require(tidyr)
 require(dplyr)
 require(ggplot2)
+require(stringr)
+
+ConvertFactor <- function(x) {
+  # Converts factors with "na" levels to factor with missing values
+  # 
+  # Args:
+  #   x: factor vector
+  #
+  # Returns:
+  #   Factor with missing values
+  
+  lev <- levels(x)
+  lev <- lev[!lev %in% c("", "na")]
+  
+  x <- as.character(x) %>% tolower()
+  x[!x %in% lev] <- NA
+    
+  factor(x)
+}
 
 # Reads data
 .data.file <- "data/sharing_survey_results_pilot.csv"
@@ -36,5 +61,35 @@ df <- read.table(.data.file, header = T, sep = ",", fill = NA) %>%
          answer.own_income = Answer.own_income) %>%
   tbl_df()
 
+# Converts factors
+df <- df %>% mutate_each(funs(ConvertFactor), answer.own, answer.lent,
+                         answer.borrowed, answer.rent, answer.no_own_reason)
 
+# Basic analysis
+
+# Number of owners of 'input.good'
+df %>% 
+  select(input.good, answer.own) %>% 
+  group_by(input.good, answer.own) %>% 
+  summarise(count = n()) %>%
+  spread(answer.own, count, fill = 0) %>%
+  mutate(total = no + yes + `NA`)
+
+# Reasons for not owning 'input.good'
+df %>% 
+  select(input.good, answer.no_own_reason) %>% 
+  group_by(input.good, answer.no_own_reason) %>% 
+  summarise(count = n()) %>%
+  spread(answer.no_own_reason, count, fill = 0) %>%
+  mutate(total = expensive + little_use + space + `NA`)
+
+# Are owners of 'input.good' ever borrowed or rent it?
+df %>%
+  select(input.good, answer.own, answer.borrowed, answer.rent) %>% 
+  unite(answer.rent_or_borrowed, answer.borrowed, answer.rent, remove = T) %>%
+  mutate(answer.rent_or_borrowed = str_detect(answer.rent_or_borrowed, "yes")) %>%
+  group_by(input.good, answer.own, answer.rent_or_borrowed) %>%
+  summarise(count = n()) %>%
+  spread(answer.rent_or_borrowed, count, fill = 0) %>%
+  mutate(total = `TRUE` + `FALSE`)
 
