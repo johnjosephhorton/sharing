200bcf5e1a0d623f7cc717b2ded494496e20ffed
John Horton <john.joseph.horton@gmail.com>
2014-09-26 09:09:22 -0400

    preliminary analysis

--- code/main.R
+++ code/main.R
@@ -23,6 +23,13 @@ require(tidyr)
 require(dplyr)
 require(ggplot2)
 require(stringr)
+library(data.table)
+library(ggplot2)
+library(lme4)
+library(stargazer)
+library(scales)
+library(JJHmisc) # non-cran resource 
+
 
 ConvertFactor <- function(x) {
   # Converts factors with "na" levels to factor with missing values
@@ -43,7 +50,8 @@ ConvertFactor <- function(x) {
 }
 
 # Reads data
-.data.file <- "data/sharing_survey_results_pilot.csv"
+.data.file <- "../data/sharing_survey_results_pilot.csv"
+
 df <- read.table(.data.file, header = T, sep = ",", fill = NA) %>%
   select(worker.id = WorkerId,
          assignment.id = AssignmentId,         
@@ -58,14 +66,289 @@ df <- read.table(.data.file, header = T, sep = ",", fill = NA) %>%
          answer.granularity = Answer.granularity,
          answer.predictability = Answer.predictability,
          answer.no_own_reason = Answer.no_own_reason,
-         answer.own_income = Answer.own_income) %>%
-  tbl_df()
+         answer.own_income = Answer.own_income) %>% tbl_df()
 
 # Converts factors
 df <- df %>% mutate_each(funs(ConvertFactor), answer.own, answer.lent,
                          answer.borrowed, answer.rent, answer.no_own_reason)
 
+# ecode usage levels 
+usage.levels <- 0:11
+usage.labels <- c(
+    "We would not use this at all",
+    "1 minute a week (about 1 hour a year)",
+    "5 minutes a week (about 4 hours a year)",
+    "1/2 an hour a week",
+    "1 hour a week",
+    "1/2 an hour a day",
+    "1 hour a day",
+    "2 hours a day",
+    "4 hours a day",
+    "8 hours a day",
+    "16 hours a day",
+    "24 hours a day (I would continuously be using this good")
+
+minute.per.week <- (60/7)/(24*60*60*7)
+hour.per.day <- (1/24)
+
+day.frac <- c(0,
+              minute.per.week, 
+              5 * minute.per.week,
+              30 * minute.per.week, 
+              60 * minute.per.week,
+              0.5 * hour.per.day,
+              hour.per.day,
+              2 * hour.per.day,
+              4 * hour.per.day,
+              8 * hour.per.day,
+              15 * hour.per.day,
+              24 * hour.per.day)
+              
+df$usage <- with(df, factor(answer.usage, levels = usage.levels, labels = usage.labels))
+df$x <- as.numeric(as.character(with(df, factor(answer.usage, levels = usage.levels, labels = day.frac))))
+
 # Basic analysis
+
+
+#-------------------------------------------------
+# Fraction of respondents owning the various goods 
+#-------------------------------------------------
+
+df.by.good <- data.table(df)[, list(frac.own = mean(answer.own == "yes", na.rm = TRUE)), by = input.good]
+df.by.good$input.good <- with(df.by.good, reorder(input.good, frac.own, mean))
+
+g.own <- ggplot(data = df.by.good,
+                aes(x = input.good, y = frac.own)) +
+    geom_point() +
+    coord_flip() +
+    theme_bw() +
+    xlab("") +
+    ylab("Fraction of respondents") +
+    scale_y_continuous(label = percent)
+
+writeImage(g.own, "ownership_fractions", width = 4, height = 4, path = "../writeup/plots/")
+
+#---------------------------
+#  Ownership by oncome bands
+#---------------------------
+
+df$income.band <- with(df,
+                       cut(income.index, breaks = quantile(df$income.index, na.rm = TRUE),
+                           labels = c("0-25", "25-50", "50-75", "75-100"))
+                       )
+
+df$income.band <- with(df,
+                       cut(income.index, breaks = quantile(df$income.index, probs = seq(0, 1, 1/3), na.rm = TRUE),
+                           labels = c("Lower 1/3 of Income", "Mid Income", "Top 1/3 of Income"))
+                       )
+
+# why are the quantiles not equally sized?
+
+# stat summary 
+g.own.inc <- ggplot(data = df,
+                aes(x = input.good, y = as.numeric(own), colour = factor(income.band))) +
+    stat_summary(fun.y = mean, fun.ymin = mean, fun.ymax = mean) + 
+    coord_flip() +
+    theme_bw() +
+    xlab("") +
+    ylab("Fraction of respondents") +
+    scale_y_continuous(label = percent)
+
+
+df.tmp <- data.table(subset(df, !is.na(income.band)))
+df.tmp[, frac.own.total := mean(answer.own == "yes", na.rm = TRUE), by = input.good]
+
+df.by.good.by.inc <- df.tmp[, list(frac.own = mean(answer.own == "yes", na.rm = TRUE),
+                                   delta.own = mean(answer.own == "yes", na.rm = TRUE) - mean(frac.own.total)),
+                                    by = list(input.good, income.band)]
+
+
+# order by variance 
+df.by.good.by.inc$input.good <- with(df.by.good.by.inc, reorder(input.good, delta.own, var))
+
+g.own.inc <- ggplot(data = df.by.good.by.inc,
+                aes(x = input.good, y = delta.own)) +
+    geom_point() +
+    facet_wrap(~income.band, ncol = 4) + 
+    geom_line(aes(group = income.band)) +
+    geom_hline(yintercept = 0, colour = "red", linetype = "dashed") + 
+    coord_flip() +
+    theme_bw() +
+    xlab("") +
+    ylab("Difference in ownership % from population mean ownership") +
+    scale_y_continuous(label = percent)
+
+print(g.own.inc)
+
+writeImage(g.own.inc, "ownership_fractions_inc", width = 8, height = 8, path = "../writeup/plots/")
+
+df.by.good.by.inc$input.good <- with(df.by.good.by.inc, reorder(input.good, frac.own, mean))
+
+g.own.inc <- ggplot(data = df.by.good.by.inc,
+                aes(x = input.good, y = frac.own)) +
+    geom_point() +
+    geom_line(aes(group = income.band)) +
+    facet_wrap(~income.band, ncol = 4) + 
+    coord_flip() +
+    theme_bw() +
+    xlab("") +
+    ylab("Fraction of respondents") +
+    scale_y_continuous(label = percent)
+
+
+
+print(g.own.inc)
+
+
+writeImage(g.own.inc, "ownership_fractions_inc", width = 8, height = 8, path = "../writeup/plots/")
+
+
+
+#-------------------
+# Ownership analysis
+#-------------------
+
+df <- within(df, {
+    own = answer.own == "yes"
+    borrowed = answer.borrowed == "yes"
+    lent = answer.lent == "yes"
+    usage.index = scale(as.numeric(answer.usage))
+    income.index = scale(as.numeric(answer.own_income))
+    predict.index = scale(as.numeric(answer.predictability))
+    granular.index = scale(as.numeric(answer.granularity))
+    rent = answer.rent == "yes"
+})
+               
+m.1 <- lm(own ~ usage.index, data = df)
+
+ggplot(data = df, aes(x = usage.index, as.numeric(own))) +
+    geom_smooth() +
+    geom_point() + facet_wrap(~input.good, ncol = 4)
+
+ggplot(data = df, aes(x = usage.index)) +
+    geom_histogram() +
+    facet_wrap(~input.good, ncol = 4)
+
+df <- subset(df, !is.na(own))
+
+df$decision <- with(df, ifelse(own, "Owns", "Does not own"))
+
+g.usage.by.own <- ggplot(data = df,
+                         aes(x = decision, y = usage.index)) +
+    geom_boxplot() +
+    facet_wrap(~input.good, ncol = 4) +
+    theme_bw() +
+    xlab("")
+
+if (interactive()){
+    print(g.usage.by.own)
+}
+
+writeImage(g.usage.by.own, "usage_by_own", width = 7, height = 7, path = "../writeup/plots/")
+
+library(plyr)
+
+df$input.good <- revalue(df$input.good, c("high-end digitial camera"="high-end\ndigitial camera",
+                                          "kitchen timer (or egg timer)" = "kitchen timer\n(or egg timer)",
+                                          "portable air conditioner" = "portable\nair conditioner",
+                                          "cat carrier (for transporting cats)" = "cat carrier\n(for transporting cats)",
+                                          "back-up electric generator" = "back-up\nelectric generator",
+                                          "high-end audio headphones" = "high-end\naudio headphones")
+                         )
+
+m.1 <- lmer(own ~ usage.index + (1|input.good), data = df)
+m.2 <- lm(own ~ usage.index + income.index, data = df)
+m.3 <- lmer(own ~ usage.index + income.index + (1|worker.id), data = df)
+m.4 <- lmer(own ~ usage.index + income.index + (1|input.good), data = subset(df, answer.usage !=0))
+
+m.1.a <- lmer(own ~ usage.index + income.index + (1|input.good), data = subset(df, answer.usage != 0))
+
+m.1.b <- lmer(own ~ usage.index + income.index + (1|input.good), data = df)
+
+
+out.file <- "../writeup/tables/ownership.tex"
+s <- stargazer(m.1, m.2, m.3,
+               title = "Estimated product usage and ownership",
+               label = "tab:ownership",
+               dep.var.labels = c("Item is owned"),
+               covariate.labels = c("Usage index", "Income index")
+               )
+AddTableNote(s, out.file, note = "\\\\
+{\\footnotesize 
+\\begin{minipage}{0.90 \\linewidth}
+ \\emph{Notes:} Here are some notes.
+\\end{minipage}
+}")
+
+
+ggplot(data = df, aes(
+           x = usage,
+           fill = factor(answer.own))) +
+    facet_wrap(~input.good, ncol = 5) + 
+    geom_bar(position = "dodge") + coord_flip()
+
+#----------------------------
+# Good attributes & ownership
+#----------------------------
+
+m.1 <- lm(own ~ predict.index, data = df)
+m.2 <- lm(own ~ granular.index, data = df)
+m.3 <- lm(own ~ granular.index*predict.index, data = df)
+
+out.file <- "../writeup/tables/ownership_attr.tex"
+s <- stargazer(m.1, m.2, m.3,
+               dep.var.labels = c("Item is owned"),
+               covariate.labels = c("Unpredictability index (UI)", "UI x GI", "Granularity index (GI)"),
+               title = "Good attributes and ownership---usage predictibility and granularity",
+               label = "tab:ownership_attr"
+               )
+AddTableNote(s, out.file, note = "\\\\
+{\\footnotesize 
+\\begin{minipage}{0.90 \\linewidth}
+ \\emph{Notes:} Here are some notes.
+\\end{minipage}
+}")
+
+
+#----------------------------
+# Good attributes and lending
+#----------------------------
+
+m.1 <- lm(lent ~ predict.index, data = subset(df, own))
+m.2 <- lm(lent ~ granular.index, data = subset(df, own))
+
+#----------
+# Borrowing 
+#----------
+
+m.1 <- lm(borrowed ~ usage.index + income.index, data = subset(df, !own))
+m.2 <- lm(borrowed ~ granular.index*predict.index, data = subset(df, !own))
+
+#--------
+# Renting 
+#--------
+
+m.1 <- lm(rent ~ usage.index + income.index, data = subset(df, !own))
+
+g.reasons <- ggplot(data = subset(df, !is.na(answer.no_own_reason)),
+       aes(x = answer.no_own_reason)) +
+    geom_histogram() +
+    facet_wrap(~input.good, ncol = 4) +
+    coord_flip()
+
+writeImage(g.reasons, "reasons", width = 9, height = 10, path = "../writeup/plots/")
+
+# The less predictable, the more like a good is to be owned 
+m <- lm(I(answer.own == "yes") ~ as.numeric(answer.predictability), data = df)
+
+# the more it is broken into small chunks 
+m <- lm(I(answer.own == "yes") ~ scale(as.numeric(answer.granularity)), data = df)
+
+
+m <- lm(I(answer.lent == "yes") ~ as.numeric(answer.predictability), data = subset(df, answer.own == "yes"))
+
+
+
 
 # Number of owners of 'input.good'
 df %>% 
